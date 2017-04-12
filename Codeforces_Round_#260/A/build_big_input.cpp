df2d3f521ed6a9f68ea9be4c63e6f4550ceb760f
Evgeny Proydakov <e.proydakov@rambler-co.ru>
2017-04-12 15:35:57 +0300

    Using big_num in 260:A

---
+++ Codeforces_Round_#260/A/build_big_input.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << 100000 << std::endl;
+    for(int i = 1; i < 100000; i++) {
+        std::cout << 100000 << ' ';
+    }
+    std::cout << 100000 << std::endl;
+
+    return 0;
+}
