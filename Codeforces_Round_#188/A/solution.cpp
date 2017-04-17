9fe4f800895ec1d8b0e8e4475742d0ecf424ba46
Evgeny Proydakov <e.proydakov@rambler-co.ru>
2017-04-17 16:40:34 +0300

    Complete 188:A

--- Codeforces_Round_#188/A/solution.cpp
+++ Codeforces_Round_#188/A/solution.cpp
@@ -1,12 +1,29 @@
+#include <cstdint>
 #include <iostream>
 
 int main()
 {
-    int n; int k;
+    uint64_t n; uint64_t k;
     std::cin >> n;
     std::cin >> k;
 
-    if(k > n )
+    if(n % 2 == 1) {
+        n++;
+    }
+
+    uint64_t val = 0;
+
+    uint64_t half = n / 2;
+    if(k <= half) {
+        uint64_t index = k;
+        val = 2 * index - 1;
+    }
+    else {
+        uint64_t index = k - half;
+        val = 2 * index;
+    }
+
+    std::cout << val << std::endl;
 
     return 0;
 }
