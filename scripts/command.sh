5b377acf447d2f4de7fabbb1e8b293fdc6d9af48
Evgeny Proydakov <e.proydakov@gmail.com>
2018-07-08 18:44:54 +0300

    Updated commands.sh

--- scripts/command.sh
+++ scripts/command.sh
@@ -1,1 +1,23 @@
 alias ccode="clang++ -std=c++14 -m32 -O2 solution.cpp -o solution"
+
+cinit() {
+    export SOLUTION=solution.cpp
+
+    echo "#include <iostream>"                    > $SOLUTION
+    echo ""                                      >> $SOLUTION
+    echo "int main()"                            >> $SOLUTION
+    echo "{"                                     >> $SOLUTION
+    echo "    std::ios::sync_with_stdio(false);" >> $SOLUTION
+    echo ""                                      >> $SOLUTION
+    echo ""                                      >> $SOLUTION
+    echo ""                                      >> $SOLUTION
+    echo "    return 0;"                         >> $SOLUTION
+    echo "}"                                     >> $SOLUTION
+
+    if ! [ -z $1 ]; then
+        for (( i=1; i<=$1; i++ ))
+	do
+            touch input_$i.txt
+	done;
+    fi
+}
