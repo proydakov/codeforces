712b4d264fb573daf297a4a5ddeb62a638a2788a
Evgeny Proydakov <e.proydakov@gmail.com>
2020-01-25 18:33:35 +0300

    Added 613 : A

---
+++ Codeforces_Round_#613/A/solution.cpp
@@ -0,0 +1,14 @@
+#include <cstdint>
+#include <iostream>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+
+    int n;
+    std::cin >> n;
+
+    std::cout << n + 1 << std::endl;
+
+    return 0;
+}
