e7a29d545e61b63ceb48ecaf924f3ed6a67a3666
Evgeny Proydakov <e.proydakov@gmail.com>
2020-01-25 18:29:00 +0300

    Added 494 : A

--- Codeforces_Round_#494/A/solution.cpp
+++ Codeforces_Round_#494/A/solution.cpp
@@ -1,10 +1,23 @@
+#include <array>
 #include <iostream>
+#include <algorithm>
 
 int main()
 {
     std::ios::sync_with_stdio(false);
 
+    int n;
+    std::cin >> n;
 
+    std::array<int, 1024> data{};
+    for (int i = 0; i < n; i++)
+    {
+        int val;
+        std::cin >> val;
+        data[val]++;
+    }
+    auto const it = std::max_element(data.begin(), data.end());
+    std::cout << *it << std::endl;
 
     return 0;
 }
