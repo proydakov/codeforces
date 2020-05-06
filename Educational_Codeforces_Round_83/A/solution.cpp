46fff2291bc0777c8306fc76ca2fab09f93f171e
Evgeny Proydakov <e.proydakov@gmail.com>
2020-05-06 14:14:59 +0300

    Added ECR83:A

---
+++ Educational_Codeforces_Round_83/A/solution.cpp
@@ -0,0 +1,26 @@
+#include <cstdint>
+#include <iostream>
+
+void solve()
+{
+    int n, m;
+    std::cin >> n >> m;
+
+    std::cout << (n % m == 0 ? "YES" : "NO") << "\n";
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+    for(int i = 0; i < t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
