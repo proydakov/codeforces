8b1aca03c65a5da8accb545cde06c10b5af4e3e2
Evgeny Proydakov <e.proydakov@gmail.com>
2020-05-31 16:39:52 +0300

    Added 633:A

---
+++ Codeforces_Round_#633/A/solution.cpp
@@ -0,0 +1,25 @@
+#include <cstdint>
+#include <iostream>
+
+void solve()
+{
+    int64_t n;
+    std::cin >> n;
+    std::cout << n << '\n';
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+    for(int i = 0; i < t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
