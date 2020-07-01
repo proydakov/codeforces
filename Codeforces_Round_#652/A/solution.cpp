f805533283c62784bcbf841084470f2c7054ee10
Evgeny Proydakov <e.proydakov@gmail.com>
2020-07-01 18:25:56 +0300

    Added d/repository/codeforces/Codeforces_Round_#652/A

---
+++ Codeforces_Round_#652/A/solution.cpp
@@ -0,0 +1,33 @@
+#include <cstdint>
+#include <iostream>
+
+void solve()
+{
+    long n;
+    std::cin >> n;
+
+    if (n % 4 == 0)
+    {
+        std::cout << "YES\n";
+    }
+    else
+    {
+        std::cout << "NO\n";
+    }
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+    for(int i = 0; i < t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
