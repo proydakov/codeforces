3e76d0d44f0430331c808567b2ad3eb5476d4d3f
Evgeny Proydakov <e.proydakov@gmail.com>
2020-09-04 21:42:10 +0300

    Renamed code -> algorithms

--- algorithms/eratosthenes_sieve.h
+++ algorithms/eratosthenes_sieve.h
@@ -1,16 +1,13 @@
 #include <vector>
 
-template<size_t n>
-std::vector<bool> build_eratosthenes_sieve()
+std::vector<bool> build_eratosthenes_sieve(size_t n)
 {
     std::vector<bool> prime(n + 1, 1);
     prime[0] = prime[1] = 0;
     for (int i = 2; i * i <= n; i++) {
         if (prime[i]) {
             for (int j = 2; j <= n / i; j++) {
-                if (prime[i * j]) {
-                    prime[i * j] = 0;
-                }
+                prime[i * j] = 0;
             }
         }
     }
