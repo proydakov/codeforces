e3ea38cf6633721b882c2dc8e1ab7da2979ef355
Evgeny Proydakov <e.proydakov@gmail.com>
2020-09-04 21:24:49 +0300

    Added templates folder

---
+++ templates/1.cpp
@@ -0,0 +1,13 @@
+#include <cstdint>
+#include <iostream>
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+
+
+    return 0;
+}
---
+++ templates/2.cpp
@@ -0,0 +1,23 @@
+#include <cstdint>
+#include <iostream>
+
+void solve()
+{
+
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+    for(int i = 0; i < t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
