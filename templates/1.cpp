c3870580efee865c95296ce351553428af4ce355
Evgeny Proydakov <e.proydakov@gmail.com>
2020-11-21 22:14:48 +0300

    Updated template 1.cpp

--- templates/1.cpp
+++ templates/1.cpp
@@ -1,13 +1,18 @@
 #include <cstdint>
 #include <iostream>
 
+void solve()
+{
+
+}
+
 int main()
 {
     std::ios::sync_with_stdio(false);
     std::cin.tie(nullptr);
     std::cout.tie(nullptr);
 
-
+    solve();
 
     return 0;
 }
