1545c3875709d44d3aee560573da77b5ddb20ea1
Evgeny Proydakov <e.proydakov@gmail.com>
2021-09-05 15:19:27 +0300

    Added /Users/evgeny.proydakov/repository/codeforces/Harbour.Space_Scholarship_Contest_2021-2022/A

---
+++ Harbour.Space_Scholarship_Contest_2021-2022/A/solution.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <cstdint>
+#include <iostream>
+#include <algorithm>
+
+void solve()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::cout << (n + 1) / 10 << "\n";
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+    for(int i = 0; i < t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
