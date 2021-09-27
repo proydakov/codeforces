7f2054803362d108a11f75fe581d959a585eec34
Evgeny Proydakov <e.proydakov@gmail.com>
2021-09-27 12:04:17 +0300

    Added /Users/evgeny.proydakov/repository/codeforces/Codeforces_Round_#363/A

--- Codeforces_Round_#363/A/solution.cpp
+++ Codeforces_Round_#363/A/solution.cpp
@@ -4,36 +4,65 @@
 #include <iostream>
 #include <algorithm>
 
+enum
+{
+    RELAX = 0,
+    CONT = 1,
+    GYM  = 2,
+
+    SIZE
+};
+
+void solve()
+{
+    size_t n;
+    std::cin >> n;
+
+    using day_t = int;
+    std::vector<day_t> days(n + 1);
+    for(size_t i = 1; i <= n; i++)
+    {
+        std::cin >> days[i];
+    }
+
+    using cell_t = int;
+    std::vector<std::vector<cell_t>> dp(n + 1, std::vector<cell_t>(SIZE, 0));
+
+    for(size_t i = 1; i <= n; i++)
+    {
+        dp[i][RELAX] = std::min({dp[i - 1][RELAX], dp[i - 1][CONT], dp[i - 1][GYM]}) + 1;
+
+        auto const day = days[i];
+        if (day == 1 || day == 3)
+        {
+            dp[i][CONT] = std::min(dp[i - 1][RELAX], dp[i - 1][GYM]);
+        }
+        else
+        {
+            dp[i][CONT] = std::numeric_limits<cell_t>::max();
+
+        }
+
+        if (day == 2 || day == 3)
+        {
+            dp[i][GYM] = std::min(dp[i - 1][RELAX], dp[i - 1][CONT]);
+        }
+        else
+        {
+            dp[i][GYM] = std::numeric_limits<cell_t>::max();
+        }
+    }
+
+    std::cout << std::min({dp[n][RELAX], dp[n][CONT], dp[n][GYM]}) << '\n';
+}
+
 int main()
 {
     std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
 
-    int n;
-    std::cin >> n;
-
-    std::string dirs;
-    std::cin >> dirs;
-
-    std::vector<int64_t> particles(n);
-    for(int i = 0; i < n; i++) {
-        std::cin >> particles[i];
-    }
-
-    int64_t distance = std::numeric_limits<int64_t>::max();
-    for(auto n = dirs.find("RL"); n != std::string::npos; n = dirs.find("RL", n + 2)) {
-        const int64_t R = particles[n + 0];
-        const int64_t L = particles[n + 1];
-
-        const int64_t test_distance = (L - R) / 2;
-        distance = std::min(distance, test_distance);
-    }
-
-    if(std::numeric_limits<int64_t>::max() == distance) {
-        std::cout << -1 << std::endl;
-    }
-    else {
-        std::cout << distance << std::endl;
-    }
+    solve();
 
     return 0;
 }
