e429ae7bc7ef4e077eddd911734cab6079f289bf
Evgeny Proydakov <e.proydakov@gmail.com>
2021-10-21 20:20:57 +0300

    Added /Users/evgeny.proydakov/repository/codeforces/Codeforces_Round_#682/A

---
+++ Codeforces_Round_#682/A/solution.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <cstdint>
+#include <iostream>
+#include <algorithm>
+
+void solve()
+{
+    size_t n;
+    std::cin >> n;
+
+    constexpr size_t counter = 1;
+    for(size_t i = 0; i < n; i++)
+    {
+        std::cout << counter << ' ';
+    }
+    std::cout << '\n';
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+    for(int i = 0; i < t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
