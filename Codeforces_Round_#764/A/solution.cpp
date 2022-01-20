930aadabf288fd0269d69caf2a5886517aa06acd
Evgeny Proydakov <e.proydakov@gmail.com>
2022-01-20 19:53:53 +0300

    Added /Users/evgeny.proydakov/repository/codeforces/Codeforces_Round_#764/A

---
+++ Codeforces_Round_#764/A/solution.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <cstdint>
+#include <iostream>
+#include <algorithm>
+
+void solve()
+{
+    size_t n;
+    std::cin >> n;
+
+    std::vector<std::int64_t> vec(n);
+    for(auto& e : vec) std::cin >> e;
+
+    const auto [min, max] = std::minmax_element(vec.begin(), vec.end());
+    std::cout << *max - *min << "\n";
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+    for(int i = 0; i < t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
