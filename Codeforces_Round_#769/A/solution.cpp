2b6e024683517a44ce835ab46807f2f7912bfea1
Evgeny Proydakov <e.proydakov@gmail.com>
2022-02-20 18:01:41 +0300

    Added /Users/evgeny.proydakov/repository/codeforces/Codeforces_Round_#769/A

---
+++ Codeforces_Round_#769/A/solution.cpp
@@ -0,0 +1,38 @@
+#include <string>
+#include <vector>
+#include <cstdint>
+#include <iostream>
+#include <algorithm>
+
+void solve()
+{
+    size_t n;
+    std::string str;
+    std::cin >> n;
+    std::cin >> str;
+
+    if (n == 1 || str == "01" || str == "10")
+    {
+        std::cout << "YES\n";
+    }
+    else
+    {
+        std::cout << "NO\n";
+    }
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+    for(int i = 0; i < t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
