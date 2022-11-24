bb65036f15724bd369b5189f158c2e80615c4cce
Evgeny Proydakov <e.proydakov@gmail.com>
2022-11-24 10:48:33 +0300

    Added /Users/evgeny.proydakov/repository/codeforces/Codeforces_Round_#835/A

---
+++ Codeforces_Round_#835/A/solution.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <cstdint>
+#include <iostream>
+#include <algorithm>
+
+void solve()
+{
+    std::vector<int64_t> vec(3);
+    for(auto& e : vec) std::cin >> e;
+
+    std::sort(vec.begin(), vec.end());
+
+    std::cout << vec[1] << '\n';    
+}
+
+int main()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+
+    int t;
+    std::cin >> t;
+    for(int i = 0; i < t; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
